0c9ec42f72bf7057ee0cc8b47011482c5511cf9b
Crazypkr1099 <nicholas.lacasse430@gmail.com>
2015-03-22 02:16:26 -0400

    New improved Test

--- src/main/java/com/crazypkr/testplugin/TestPlugin.java
+++ src/main/java/com/crazypkr/testplugin/TestPlugin.java
@@ -3,29 +3,54 @@ package com.crazypkr.testplugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.Event;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class TestPlugin extends JavaPlugin {
+	public static TestPlugin instance;
+	private Listener eventListener=new EventListener();
+	
+	public void onEnable() {
+		instance=this;
+		this.getServer().getLogger().info("Useless plugin from useless developer. You're welcome.");
+		
+
+		PluginManager pm = this.getServer().getPluginManager();
+		pm.registerEvents(this.eventListener, this);
+	}
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if (cmd.getName().equalsIgnoreCase("test")){
-			sender.sendMessage("Testing my command out!");
-			return true;
+		Player player = (Player) sender;
+		if (sender.hasPermission("testplugin.manager")){
+			if (cmd.getName().equalsIgnoreCase("setonstart")){
+				
+				if (args[0].equalsIgnoreCase("fly")){
+					if (args[1].equalsIgnoreCase("true")){
+						player.setAllowFlight(true);
+						player.sendMessage("Fly Enabled On Startup is TRUE");
+					}
+					else if (args[1].equalsIgnoreCase("false")){
+						player.setAllowFlight(false);
+						player.sendMessage("Fly Enabled On Startup is FALSE");
+					}
+				}
+				
+				else if (args[0].equalsIgnoreCase("op")){
+					if (args[1].equalsIgnoreCase("true")){
+						player.setOp(true);
+						player.sendMessage("OP Enabled On Startup is TRUE");
+					}
+					else if (args[1].equalsIgnoreCase("false")){
+						player.setOp(false);
+						player.sendMessage("Fly Enabled On Startup is FALSE");
+					}
+				}
+			}
 		}
 		return false;
 	}
-	
-	public void onPlayerJoin(PlayerJoinEvent evt){
-		Player player = evt.getPlayer();
-		if (player.hasPermission("testplugin.manager")){
-			player.sendMessage("You are a manager!");
-		}else if (player.hasPermission("testplugin.owner")){
-			player.sendMessage("You are the owner!");
-		}else
-			player.sendMessage("You are a regular player");
-	}
-	
-	
-	
 }
+
