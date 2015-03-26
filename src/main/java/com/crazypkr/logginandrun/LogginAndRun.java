26c2fdcefe865df2dc4f74076dc37c5b43b649dd
Crazypkr1099 <Nicholas Lacasse@CyberproNL>
2015-03-26 18:34:15 -0400

    Updated
    Use /os <cmd> <on/off> to turn certain command on/off
    
    Use /os on/off to turn all on/off
    
    Use /osc to check recent commands added

--- src/main/java/com/crazypkr/logginandrun/LogginAndRun.java
+++ src/main/java/com/crazypkr/logginandrun/LogginAndRun.java
@@ -1,7 +1,6 @@
 package com.crazypkr.logginandrun;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -21,7 +20,6 @@ public final class LogginAndRun extends JavaPlugin {
 	
 	public void onEnable() {
 		instance=this;
-		this.getServer().getLogger().info("Useless plugin from useless developer. You're welcome.");
 		
 
 		PluginManager pm = this.getServer().getPluginManager();
@@ -30,74 +28,127 @@ public final class LogginAndRun extends JavaPlugin {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		Player player = (Player) (sender);
+		ArrayList<String> usersCArray = userCommands.get(player.getUniqueId());
+		String ustrcommand = "";
+		String onoff = null;
 		if (sender.hasPermission("logginandrun.manager")){
-			if (cmd.getName().equalsIgnoreCase("setonstart")){
-				String ustrcommand = "";
-				for (String s:args){
-					ustrcommand += s + " ";
+			if (cmd.getName().equalsIgnoreCase("onstart")){
+				for (int i = 0; i < args.length-1; i++){
+					ustrcommand += args[i] + " ";
 				}
-				ArrayList<String> usersCArray = userCommands.get(player.getUniqueId());
-				if (usersCArray == null){
-					usersCArray = new ArrayList<String>();
-					userCommands.put(player.getUniqueId(),usersCArray);
-				}
-				usersCArray.add(ustrcommand);
-				player.sendMessage(ChatColor.YELLOW + "Added " + ustrcommand + "to run on login");
-			}
-			if (cmd.getName().equalsIgnoreCase("onstartcommands")){
-					ArrayList<String> commandlist = userCommands.get(player.getUniqueId());
-					if (commandlist == null || commandlist.size() == 0){
-						player.sendMessage(ChatColor.YELLOW + "+-+-+-+-+-+-+-+-+-+-+-+\n" +
-								ChatColor.RED + "Theres no commands being\n"	+
-								"runned on log in\n"		+
-								ChatColor.GREEN + "Use /setonstart <cmd>\n" +
-								"to add a command\n" + 
-								ChatColor.YELLOW + "+-+-+-+-+-+-+-+-+-+-+-+\n");
-						
-						}
-					
 				
-					else{
-						String listofcmd = "";
-						for (String s:commandlist){
-							listofcmd += s + "\n";
+				if (args[0].equalsIgnoreCase("on") || args[0].equalsIgnoreCase("off")){
+					if (usersCArray != null){
+						ArrayList<String> temp = new ArrayList<String>();
+						for (String s : usersCArray){
+							temp.add(s);
 						}
-						player.sendMessage(ChatColor.YELLOW + "+-+-+-+-+-+-+-+-+-+-+-+\n" +
-											"Loggin and Run Commands\n"	+
-											"Scheduled to be on\n"		+
-											"Login\n"					+
-											"+-+-+-+-+-+-+-+-+-+-+-+\n"	+ ChatColor.RED + ChatColor.BOLD +
-											listofcmd + "\n");
+						Iterator<String> ite = usersCArray.iterator();
+						while(ite.hasNext()){
+							String s = ite.next();
+							ite.remove();							
+						}
+						for (String s: temp){
+							player.sendMessage("hi");
+							ustrcommand = "";
+							String[] sstring = s.split(" ");
+							for (int i = 0; i < sstring.length-1; i++){
+								ustrcommand += sstring[i] + " ";
+							}
+							player.sendMessage(sstring[sstring.length-1]);
+							if (args[0].contains("on")){
+								usersCArray.add(ustrcommand + "on");
+							}
+							else if (args[0].contains("off")){
+								usersCArray.add(ustrcommand + "off");
+							}
+						}
+						temp.removeAll(temp);
+					}
+					return false;
+				}
+				if (args[args.length-1].equalsIgnoreCase("on")){
+					onoff = "on";
+				}
+				else if (args[args.length-1].equalsIgnoreCase("off")){
+					onoff = "off";
+				}
+				else{
+					onoff = null;
+				}
+				
+				if (onoff == null){
+					player.sendMessage(ChatColor.RED + "Sorry, please use /onstart <cmd> <on/off> to toggle commands");
+				}
+				else{
+					if (usersCArray == null){
+						usersCArray = new ArrayList<String>();
+						userCommands.put(player.getUniqueId(),usersCArray);
+					}
+					boolean check = true;
+					for (String s : usersCArray){
+						if (s.contains(ustrcommand)){
+							String[] sstring = s.split(" ");
+							player.sendMessage(onoff + " " + sstring);
+							player.sendMessage(onoff.length() + " " + sstring[sstring.length-1].length());
+							if (onoff.contains(sstring[sstring.length-1])){
+								player.sendMessage(ChatColor.RED + "Sorry, you already have this toggled on or off!");
+								check = false;
+							}
+						}
+					}
+					if (check == true){
+						if (onoff.equals("on")){
+							usersCArray.remove(ustrcommand + "off");
+			
+							player.sendMessage(ChatColor.RED + "Toggled " + ustrcommand + "to run on login");
+							usersCArray.add(ustrcommand + "on");
+							userCommands.put(player.getUniqueId(),usersCArray);
+						}
+						else if (onoff.equals("off")){
+							usersCArray.remove(ustrcommand + "on");
+							
+							player.sendMessage(ChatColor.RED + ustrcommand + "will not be ran on login");
+							usersCArray.add(ustrcommand + "off");
+							userCommands.put(player.getUniqueId(),usersCArray);
+						}
 					}
 				}
-			if (cmd.getName().equalsIgnoreCase("removeonstart")){
-				boolean Find = false;
-				String ustrcommand = " ";
-				for (String s:args){
-					ustrcommand += s + " ";
-				}
-				ArrayList<String> usersCArray = userCommands.get(player.getUniqueId());
-				Iterator<String> ite = usersCArray.iterator();
-				while(ite.hasNext()){
-					String value = ite.next();
-					if (ustrcommand.contains(value)){
-						ite.remove();
-						player.sendMessage(ChatColor.YELLOW + "Removed " + value + "from running on login");
-						Find = true;
-					}
-				}
-				if (Find == false){
-					player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Cannot find command " + ustrcommand + "to be runned on startup\n"+
-									   "Please use /onstartcommands to find the commands you have logged");
-					
-					
-				}
 			}
-				
 		}
+		
+		if (cmd.getName().equalsIgnoreCase("onstartcommands")){
+			ArrayList<String> commandlist = userCommands.get(player.getUniqueId());
+			if (commandlist == null || commandlist.size() == 0){
+				player.sendMessage(ChatColor.YELLOW + "+-+-+-+-+-+-+-+-+-+-+-+\n" +
+						ChatColor.RED + "Theres no commands being\n"	+
+						"ran on login\n"		+
+						ChatColor.GREEN + "Use /setonstart <cmd>\n" +
+						"to add a command\n" + 
+						ChatColor.YELLOW + "+-+-+-+-+-+-+-+-+-+-+-+\n");
+				
+				}
+			
+		
+			else{
+				String listofcmd = "";
+				for (String s:commandlist){
+					listofcmd += s + "\n";
+				}
+				player.sendMessage(ChatColor.YELLOW + "+-+-+-+-+-+-+-+-+-+-+-+\n" +
+									"Loggin and Run Commands\n"	+
+									"Scheduled to be on\n"		+
+									"Login\n"					+
+									"+-+-+-+-+-+-+-+-+-+-+-+\n"	+ ChatColor.GREEN + ChatColor.BOLD +
+									listofcmd + "\n");
+			}
+		}
+		
+		
+		
 		
 		
 		return false;
 	}
 }
-
+					
